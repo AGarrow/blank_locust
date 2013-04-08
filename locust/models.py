328ee902305fb5db0f61ece2995de94b6ae0593c
Paul Tagliamonte <paultag@sunlightfoundation.com>
2013-04-08 12:14:44 -0400

    Remove the OCD class, use Division

--- locust/models.py
+++ locust/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 
-class OpenCivicID(models.Model):
-    id = models.CharField(max_length=128, primary_key=True)
-    external_id = models.CharField(max_length=128, unique=True)
-    set_id = models.CharField(max_length=128)
-    start = models.DateTimeField()
-    end = models.DateTimeField(null=True)
-
-    def __unicode__(self):
-        return self.id
-
-
 class Division(models.Model):
     id = models.CharField(max_length=300, primary_key=True)
     display_name = models.CharField(max_length=100)
